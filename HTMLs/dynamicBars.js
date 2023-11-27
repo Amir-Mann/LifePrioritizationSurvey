22682e3af7056be1e4465f53f6c9293c9f2b3268
RavidElisabeth <93582474+RavidElisabeth@users.noreply.github.com>
2023-11-27 09:13:44 +0900

    Dynamic bars is now actually working
    But not through the .js file?
    Idk why but atleast it works

--- HTMLs/dynamicBars.js
+++ HTMLs/dynamicBars.js
@@ -1,14 +1,15 @@
 function updateRectangles(svgId, labelText, averageValue, maxValue) {
+  console.log("hej");
+
   const svg = document.getElementById(svgId);
   const percentage = (averageValue * 100).toFixed(2);
-  const barWidth = Math.min(averageValue, 1) * maxValue;
+  const barWidth = averageValue * maxValue;
   const inverseWidth = maxValue - barWidth;
-
+  console.log("Width= " + barWidth);
+  
   svg.querySelector('.results-pip-label').textContent = `${labelText}: ${percentage}%`;
   svg.querySelector('rect:nth-child(2)').setAttribute('width', barWidth);
   svg.querySelector('rect:nth-child(3)').setAttribute('width', inverseWidth);
-
-  //svg.querySelector('rect:nth-child(3)').setAttribute('x', (220 + barWidth) + '');
-  //svg.querySelector('rect:nth-child(2)').setAttribute('x', (220 + barWidth) + '');
-
+  svg.querySelector('rect:nth-child(3)').setAttribute('x', barWidth + 120);
+  // Adjust the x attribute for the second rectangle based on the barWidth and additional spacing
 }
