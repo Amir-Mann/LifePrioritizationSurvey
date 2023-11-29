6f68b1a08358da44a1b5321a8ecb40963c145674
RavidElisabeth <93582474+RavidElisabeth@users.noreply.github.com>
2023-11-29 17:21:13 +0900

    More dynamic stuff on statistics page
    Your choice is now seen, also cleaned stuff up just in general.

--- HTMLs/dynamicBars.js
+++ HTMLs/dynamicBars.js
@@ -1,15 +1,19 @@
-function updateRectangles(svgId, labelText, averageValue, maxValue) {
-  console.log("hej");
-
+function updateRectangles(svgId, labelText, averageValue, maxValue, yourChoice) {
   const svg = document.getElementById(svgId);
   const percentage = (averageValue * 100).toFixed(2);
   const barWidth = averageValue * maxValue;
   const inverseWidth = maxValue - barWidth;
-  console.log("Width= " + barWidth);
+  console.log("Width= " + yourChoice);
   
   svg.querySelector('.results-pip-label').textContent = `${labelText}: ${percentage}%`;
   svg.querySelector('rect:nth-child(2)').setAttribute('width', barWidth);
   svg.querySelector('rect:nth-child(3)').setAttribute('width', inverseWidth);
   svg.querySelector('rect:nth-child(3)').setAttribute('x', barWidth + 120);
+
+  if (yourChoice == 0) {
+    svg.querySelector('.choice').setAttribute('x', '571.5');
+  } else {
+    svg.querySelector('.choice').setAttribute('x', '60');
+  }
   // Adjust the x attribute for the second rectangle based on the barWidth and additional spacing
 }
